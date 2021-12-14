2ded9d995cbf750e0f4b727527b954bda72008c9
lokesh <kottelokesh789@gmail.com>
2021-12-14 11:53:19 +0530

    initialcommit

---
+++ src/com/bridgelabz/basiccoreprograms/FlipCoin.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class FlipCoin {
+
+	public static void main(String[] args) {
+		int numOfFilp = 10;
+		int heads = 0;
+	    int tails = 0;
+	    for(int i = 1; i <= numOfFilp;i++) {
+	    	if(Math.random() < 0.5) {
+	    		tails++;
+	    	}
+	    
+	 	    }
+	    double precentageOftails = (tails * 100)/ numOfFilp;
+	    double precentageOfheads = 100 - precentageOftails;
+	    System.out.println(precentageOftails);
+	    System.out.println(precentageOfheads);
+
+	}
+
+}
